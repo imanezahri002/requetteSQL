0193d615ae6d77213c9d4f12f3458db36e8a1206
imanezahri002 <imanezh5@gmail.com>
2024-12-10 14:56:26 +0100

    new creation du database

--- bd.sql
+++ bd.sql
@@ -1,72 +1,59 @@
---NAME: imane zahri
---EMAIL: imanezahri4@gmail.com
+-- name:ZAHRI IMANE
+-- email:imanezahri4@gmail.com
 
+-- creation dB
+CREATE DATABASE streamovies;
 
-CREATE DATABASE streamfilms;
+-- utilisation dB pour  creation des tables
+USE streamovies;
 
---creation de la table user
-CREATE TABLE users (
-userId INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-firstName VARCHAR(100) NOT NULL,
-lastName VARCHAR(100) NOT NULL,
-email VARCHAR(100) UNIQUE NOT NULL,
-registrationDate DATE NOT NULL,
-subscriptionId INT NOT NULL
+-- creation 1st table subscription
+CREATE TABLE subscription(
+    subscriptionId int AUTO_INCREMENT PRIMARY KEY,
+    subscriptionType ENUM('premium','basic'),
+    monthlyFee DECIMAL(10,2) NOT NULL
 );
---creation de la table subscription
-CREATE TABLE subscription (
-    subscriptionId INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    subscriptionType VARCHAR(100) NOT NULL,
-)
---liaison du cle etrangere
-ALTER TABLE users 
-ADD CONSTRAINT idSub FOREIGN KEY (subscriptionId) REFERENCES subscription(subscriptionId)
---creation de la table review
+
+-- create 2nd table users
+CREATE TABLE users(
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    registrationDate DATE NOT NULL,
+    subscriptionId INT NOT NULL, 
+    FOREIGN KEY (subscriptionId) REFERENCES subscription(subscriptionId)
+);
+
+-- create 3rd table movie
+CREATE TABLE movie(
+    movieId INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    releaseYear INT NOT NULL, 
+    duration int NOT NULL,
+    rating VARCHAR(10) NOT NULL
+);
+
+-- create 4th table whatchistory
+CREATE TABLE whatchhistory(
+    whatchhistoryId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES users(userId),
+    movieId INT NOT NULL,
+        FOREIGN KEY (movieId) REFERENCES movie(movieId),
+    whatchDate DATE NOT NULL,
+    completionPercentage INT NOT NULL DEFAULT 0
+);
+
+-- create the last table review
 CREATE TABLE review(
-reviewId INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-userId INT NOT NULL,
-movieId INT NOT NULL,
-rating INT NOT NULL,
-reviewText TEXT NOT NULL,
-reviewDate DATE NOT NULL
+    reviewId INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES users(userId),
+    movieId INT NOT NULL,
+        FOREIGN KEY (movieId) REFERENCES movie(movieId),
+    rating INT NOT NULL DEFAULT 0,
+    reviewText TEXT NULL,
+    reviewDate DATE
 );
---creation de la table  movie
-CREATE TABLE movie( 
-movieId INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-title VARCHAR(255) NOT NULL,
-genre VARCHAR(100),
-releaseYear INT NOT NULL,
-duration INT NOT NULL,
-rating VARCHAR(10)NOT NULL 
-);
---ajout du cle etrangere userId a la table movie
-ALTER TABLE review
-ADD CONSTRAINT idUSER FOREIGN KEY (userId) REFERENCES users(userId)
---ajout du cle etrangere movieId a la table movie
-ALTER TABLE review
-ADD CONSTRAINT idMovie FOREIGN KEY (movieId) REFERENCES movie(movieId)
--- modification sur type data du champ reviewText
-ALTER TABLE review 
-MODIFY COLUMN reviewText TEXT NULL;
---creation de la table watchhistory
-CREATE TABLE watchhistory(
-watchhistoryId INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-userId INT NOT NULL,
-movieId INT NOT NULL,
-watchDate DATE NOT NULL,
-completionPercentage INT DEFAULT 0
-)
---ajout de la contrainte de la cle etrangere 
-ALTER TABLE watchhistory
-ADD CONSTRAINT movieid FOREIGN KEY (movieId) REFERENCES movie(movieId)
-
---ajout de la contrainte de la cle etrangere 
-ALTER TABLE watchhistory
-ADD CONSTRAINT userid FOREIGN KEY (userId) REFERENCES users(userId)
--- modification du type du colonne subscriptionType
-ALTER TABLE subscription
-MODIFY COLUMN subscriptionType ENUM('basics','premium') NOT NULL;
---ajout du champ monthlyFee dans la table subscription
-ALTER TABLE subscription
-ADD COLUMN monthlyFee DECIMAL(10,2) NOT NULL 
-
